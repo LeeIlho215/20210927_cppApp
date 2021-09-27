a32961847fe9fbb35fd8d4b49d951d443c84ecce
이일호 <so0214n1@naver.com>
2021-09-27 10:38:50 +0900

    Main.cpp의 printInt 함수 완성

--- ex0927/Main.cpp
+++ ex0927/Main.cpp
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello World!\n");
-	printf("Hello World!\n");
-	printf("Hello World!\n");
-	printf("Hello World!\n");
-	printf("Hello World!\n");
+void printInt() {
+	for (int i = 0; i < 100; i++) {
+		printf("Hello : %d\n", i);
+	}
 }
+
+int main() {
+	printInt();
+}
+
+//#include <stdio.h>
+//
+//int main() {
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+//	printf("Hello World!\n");
+//}
